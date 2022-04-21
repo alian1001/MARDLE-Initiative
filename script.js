9a7a000aae83a714650110de783ae19fd8a67539
Alian <71174590+alian1001@users.noreply.github.com>
2022-04-21 10:55:31 +0800

    Update
    New Skeleton FIles

---
+++ script.js
@@ -0,0 +1,10 @@
+//Text box refrence
+var textBox = document.getElementById("textBox");
+//Button refrence
+var button = document.getElementById("button");
+
+//change button color when clicked
+button.addEventListener("click", function(event){
+    button.style.backgroundColor = "red";
+}
+);
