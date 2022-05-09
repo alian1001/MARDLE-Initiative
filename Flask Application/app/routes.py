50ac567e14d6ba269aa04ce204cb10e1c830de84
Alian <71174590+alian1001@users.noreply.github.com>
2022-05-09 23:02:22 +0800

    Push to Origin
    Created bootstrap within flask and link up all necessary pages

--- Flask Application/app/routes.py
+++ Flask Application/app/routes.py
@@ -3,6 +3,13 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     return render_template("index.html")
+
+@app.route('/game')
+def game():
+    return render_template("game.html")
+
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
